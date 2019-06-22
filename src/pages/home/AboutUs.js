e9cc887350c219bf29d2f316fd086b4b1ab1616c
Shreyes Seshasai <shreyes19@gmail.com>
2019-06-22 15:23:03 -0700

    Updating content and style on About Us page.
     Your branch is ahead of 'origin/master' by 1 commit.

--- src/pages/home/AboutUs.js
+++ src/pages/home/AboutUs.js
@@ -8,24 +8,53 @@ export default class AboutUs extends React.Component {
       <div id={"about-us"} className={"row blue-sky"}>
         <ImageColumn image={aboutUs} />
         <div className={"column"} style={{ textAlign: "center" }}>
-          <h3 style={{ color: "#EEF4F8" }}>about<span style={{ color: "#D34E23" }}>us</span></h3>
+          <h3 style={{ color: "#EEF4F8" }}>
+            about<span style={{ color: "#D34E23" }}>us</span>
+          </h3>
           <p>
-            We're a grassroots organization supporting the Pete for America campaign.  Our focus is from Palo Alto to
-            San Jose — though anyone is welcome.  If you're excited about Pete and want get involved, we have a number
-            of upcoming events such as:
+            We're a grassroots organization supporting the Pete for America
+            campaign. Our focus is from Palo Alto to San Jose — though anyone is
+            welcome. If you're excited about Pete and want get involved, we have
+            a number of upcoming events such as:
           </p>
           <ul className={"center-ul"}>
-            <li><h4>Chapter Meeting - May 14</h4></li>
-            <li><h4>SFPride Parade - Jun 30</h4></li>
-            <li><h4>SJPride Parade - Aug 24</h4></li>
+            <li>
+              <h4>
+                <a
+                  href={
+                    "https://www.mobilize.us/peteforamerica/event/96385/?fbclid=IwAR02_UGgtImG1kszPoGf-GUJ51UEV3hkAg0UxaIf-5GepFadSWTafhu0DYk&rname=Natasha&share_context=event-detail-page-modal&share_medium=copy_link"
+                  }
+                >
+                  Debate Watch Party
+                </a>{" "}
+                - Jun 27
+              </h4>
+            </li>
+            <li>
+              <h4>SFPride Parade - Jun 30</h4>
+            </li>
+            <li>
+              <h4>SJPride Parade - Aug 24</h4>
+            </li>
           </ul>
-          <p>We organize our events through the SV for Pete Meetup.  Click below to get directed there and sign up!</p>
+          <p>
+            We organize our events through the SV for Pete Meetup. Click below
+            to get directed there and sign up!
+          </p>
           <div className={"about-us-button-container"}>
-            <a href={"https://www.meetup.com/SV-for-Pete-2020/"} className={"about-us-links"} target={"_blank"}>Meetup</a>
-            <a href={"https://www.meetup.com/SV-for-Pete-2020/"} className={"about-us-links"}>Explore Collateral</a>
+            <a
+              href={"https://www.meetup.com/SV-for-Pete-2020/"}
+              className={"about-us-links"}
+              target={"_blank"}
+            >
+              Meetup
+            </a>
+            <a href={"/collateral"} className={"about-us-links"}>
+              Explore Collateral
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,16 +8,23 @@ export default class Home extends React.Component {
       <div id={"home"} className={"row river-blue"}>
         <ImageColumn fixed image={petePortrait} />
         <div className={"column"}>
-          <h1>Silicon Valley for Pete</h1>
-          <h4 style={{ marginTop: "auto", marginBottom: "0" }}>Get involved today!</h4>
+          <h2>Silicon Valley for Pete</h2>
+          <h4 style={{ marginTop: "auto", marginBottom: "0" }}>
+            Get involved today!
+          </h4>
           <p>
-            We are a grassroots organization supporting Pete Buttigieg for President in 2020. Getting involved is
-            easy. Check all the options you are interested in and we'll provide details once we know you're in. If
-            you just want to be in the loop and kept informed - that's okay too!
+            We are a grassroots organization supporting Pete Buttigieg for
+            President in 2020. Getting involved is easy. Check all the options
+            you are interested in and we'll provide details once we know you're
+            in. If you just want to be in the loop and kept informed - that's
+            okay too!
           </p>
-          <div id={'can-form-area-join-silicon-valley-for-pete'} style={{ width: "100%" }} />
+          <div
+            id={"can-form-area-join-silicon-valley-for-pete"}
+            style={{ width: "100%" }}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
